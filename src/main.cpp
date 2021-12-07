7cf1bff05280de007cf030a0db80d3c9e6a20822
John Bekendam <john@bekendam.net>
2021-12-07 21:22:44 +0100

    Improved comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-//#include <Arduino.h>
-
 #include "CommandInterface.h"
 #include "Config.h"
 #include "System.h"
@@ -8,12 +6,13 @@ CCommandInterface ci(5000);
 
 void setup()
 {
-  // Use code below to sey your wifi credentials directly without using the accesspoint
+  // Use code below to set your wifi credentials directly without using the accesspoint
   // Don't forget to remove this code before releasing.
   // Config::Load();
   // Config::SetSsid("ssid");
   // Config::SetPassword("password");
   // Config::SetUdpLogger({{10,0,0,30},5001});
+  // Config::.... possibly add more configuration parameters here
   // Config::Save();
 
   CSystem::Get().Initialize();
